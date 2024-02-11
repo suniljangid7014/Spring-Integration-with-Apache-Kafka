d2ece8a3f598aa2d35839dc46493bbc88a2d9f0b
Sunil Kumar <jangidsunil7014@gmail.com>
2024-02-11 23:07:29 +0530

    First commit

---
+++ END_USER_APP/src/main/java/com/infy/EndUserAppApplication.java
@@ -0,0 +1,13 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndUserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndUserAppApplication.class, args);
+	}
+
+}
---
+++ END_USER_APP/src/main/java/com/infy/config/AppConstant.java
@@ -0,0 +1,8 @@
+package com.infy.config;
+
+public class AppConstant {
+	
+	public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+	public static final String GROUP_ID = "group-1";
+
+}
